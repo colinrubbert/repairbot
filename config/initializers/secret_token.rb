a99366e387c20dceaa73db79ba418e6ba70b5bc6
Colin Rubbert <colinrubbert@gmail.com>
2015-06-01 01:30:58 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Repairbot::Application.config.secret_key_base = '84eabf3675307e2351f781bcd10a03d615a3085485700e7067adfcc11cbb3971980a44387010ea433b2e7cb1ec2905b99c3aacf7d6ed33e1146046ce10a91f57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Repairbot::Application.config.session_store :cookie_store, key: '_repairbot_session'
