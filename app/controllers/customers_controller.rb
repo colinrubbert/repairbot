7b056a02afc2b718d3785f2f9ede2bc9744da5b7
Colin Rubbert <colinrubbert@gmail.com>
2015-06-04 00:57:27 +0000

    Create customers controller methods (new, create, and customer_params)

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,4 +8,17 @@ class CustomersController < ApplicationController
     @customer = Customer.new
   end
 
+  def create
+    Customer.create(customer_params)
+    redirect_to customers_path
+  end
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:cname, :cpphone, :csaddr, :ccity, :cstate, :czip, :cemail)
+  end
+
+
 end
