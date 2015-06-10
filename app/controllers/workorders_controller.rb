862f6312fd6e24ddf641877827ca4193b1070574
Colin Rubbert <colinrubbert@gmail.com>
2015-06-10 02:59:20 +0000

    Add 'new' & 'create' function to workorders controller to create a new workorder, build form for new workorder

--- app/controllers/workorders_controller.rb
+++ app/controllers/workorders_controller.rb
@@ -5,7 +5,19 @@ class WorkordersController < ApplicationController
   end
 
   def new
+    @customer = Customer.find(params[:customer_id])
     @workorder = Workorder.new
+  end
+
+  def create
+    Workorder.create(workorder_params)
+    redirect_to customer_workorders_path
+  end
+
+  private
+
+  def workorder_params
+    params.require(:workorder).permit(:cdmake, :cdmodel, :cdaccr, :cdesc, :cdpass, :ccat)
   end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ActiveRecord::Base
   belongs_to :tech
   has_many :workorders
+  accepts_nested_attributes_for :workorders
 end
